90464aaa07341fe6f49b0e773a2e4bf46ab182b7
Jan Maria Kroissenbrunner <janmak98@physik.fu-berlin.de>
2023-05-16 11:38:08 +0200

    fixed tests

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -1,7 +1,7 @@
-from config import Config
-from topology import Topology
+from mucus.config import Config
+from mucus.topology import Topology
 
-cfg_path = "connected-S-mesh/parameters/param_connected-S-mesh.toml"
+cfg_path = "tests/connected-S-mesh/parameters/param_connected-S-mesh.toml"
 cfg = Config.from_toml(cfg_path)
 top = Topology(cfg)
 

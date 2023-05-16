c37319559e3d367a8ce5a46f88b292dd2cc1c55e
Jan Maria Kroissenbrunner <janmak98@physik.fu-berlin.de>
2023-05-16 11:02:55 +0200

    added pydantic to dependencies

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,7 +1,7 @@
-from system import System
-from config import Config
+from mucus.system import System
+from mucus.config import Config
 
-cfg_path = "/home/janmak98/mucus/mucus/tests/test_files/connected-S-mesh/configs/cfg_connected-S-mesh.toml"
+cfg_path = "connected-S-mesh/configs/cfg_connected-S-mesh.toml"
 cfg = Config.from_toml(cfg_path)
 sys = System(cfg)
 print("simulating...")
--- tests/test_topology.py
+++ tests/test_topology.py
@@ -1,7 +1,7 @@
 from config import Config
 from topology import Topology
 
-cfg_path = "/home/janmak98/mucus/systems/test_var_params/configs/cfg_test_mucus.toml"
+cfg_path = "connected-S-mesh/parameters/param_connected-S-mesh.toml"
 cfg = Config.from_toml(cfg_path)
 top = Topology(cfg)
 
